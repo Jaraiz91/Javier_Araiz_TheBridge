fd84d30023de85f0c19d7e556a72939719fe7986
Jaraiz91 <jaraiz3@gmail.com>
2021-05-06 18:53:05 +0200

    Numpy exercises

---
+++ week3_course_python_III/day5_probability_statistics/exercise/tablero.py
@@ -0,0 +1,9 @@
+class tablero:
+    def __init__(self, fila = [["o"]*10]*10):
+        self.fila = fila
+        self.columna = columna
+    def mostrar(self):
+        for i in range(11):
+            print(self.fila)
+
+
