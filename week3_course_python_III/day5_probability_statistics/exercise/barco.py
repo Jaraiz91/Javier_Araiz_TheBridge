75d8e9919932e29feccc001aa330012846e3ebbd
Jaraiz91 <jaraiz3@gmail.com>
2021-05-09 23:22:19 +0200

    Pandas

--- week3_course_python_III/day5_probability_statistics/exercise/barco.py
+++ week3_course_python_III/day5_probability_statistics/exercise/barco.py
@@ -1,8 +1,9 @@
+
 class Barco:
 
     # función constructor
-    def init(self, nombre, resistencia):
-        self.nombre = nombre
+    def init(self, posicion, resistencia):
+        self.posicion = posicion
         self.resistencia = resistencia
 
     def f_no_flota(self):
@@ -11,14 +12,4 @@ class Barco:
         else:
             return False
 
-barco1 = Barco(nombre = "Barco1", resistencia = 2)
-barco2 = Barco(nombre = "Barco2", resistencia = 2)
-barco3 = Barco(nombre = "Barco3", resistencia = 2)
-barco4 = Barco(nombre = "Barco4", resistencia = 2)
-barco5 = Barco(nombre = "Barco5", resistencia = 3)
-barco6 = Barco(nombre = "Barco6", resistencia = 3)
-barco7 = Barco(nombre = "Barco7", resistencia = 3)
-barco8 = Barco(nombre = "Barco8", resistencia = 4)
-barco9 = Barco(nombre = "Barco9", resistencia = 4)
-barco10 = Barco(nombre = "Barco10", resistencia = 5)
 
--- week3_course_python_III/day5_probability_statistics/exercise/tablero.py
+++ week3_course_python_III/day5_probability_statistics/exercise/tablero.py
@@ -1,9 +1,15 @@
+import numpy as np
 class tablero:
-    def __init__(self, fila = [["o"]*10]*10):
-        self.fila = fila
-        self.columna = columna
-    def mostrar(self):
-        for i in range(11):
-            print(self.fila)
-
+    def __init__(self, defensa = [[["o"]*10],[["o"]*10], [["o"]*10], [["o"]*10], [["o"]*10], [["o"]
+    *10], [["o"] * 10], [["o"] * 10], [["o"] * 10], [["o"]* 10]], ataque = [[["o"]*10],[["o"]*10], [["o"]*10], [["o"]*10], [["o"]*10], [["o"]
+    *10], [["o"] * 10], [["o"] * 10], [["o"] * 10], [["o"]* 10]]):
+        self.ataque = ataque
+        self.defensa = defensa
+    def mostrar_ataque(self):
+        for x in self.ataque:
+            print(x)
+    def mostrar_defensa(self):
+        for x in self.defensa:
+            print(x)
+    
 
